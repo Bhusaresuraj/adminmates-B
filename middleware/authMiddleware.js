96bfbe50f129fa15a5b7668529ec6c6684750703
Suraj Bhusare <surajbhusare@Surajs-MacBook-Air.local>
2026-03-19 02:10:26 +0530

    admin changes for products

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -66,7 +66,10 @@ exports.protect = async (req, res, next) => {
 // Grant access to specific roles
 exports.authorize = (...roles) => {
     return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
+        // Explicitly whitelist admin roles
+        const allowedRoles = [...roles, 'admin', 'super-admin', 'sub-admin'];
+        
+        if (!allowedRoles.includes(req.user.role)) {
             return res.status(403).json({
                 success: false,
                 message: `User role '${req.user.role}' is not authorized to access this route`
